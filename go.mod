90b532085be19aed44385f4fcf01cc32f51a36d8
Sergey Peshkov <serge1peshcoff@gmail.com>
2021-02-28 14:29:16 +0300

    feat: init commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/grafana/grafana-starter-datasource-backend
+
+go 1.15
+
+require (
+	github.com/grafana/grafana-plugin-sdk-go v0.88.0
+	github.com/hashicorp/go-plugin v1.2.2 // indirect
+	github.com/prometheus/client_golang v1.9.0 // indirect
+	google.golang.org/grpc v1.35.0 // indirect
+)
---
+++ src/ConfigEditor.tsx
@@ -0,0 +1,116 @@
+import React, { ChangeEvent, PureComponent } from 'react';
+import { LegacyForms } from '@grafana/ui';
+import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
+import { DataSourceOptions, SecureJsonData } from './types';
+
+const { SecretFormField, FormField } = LegacyForms;
+
+interface Props extends DataSourcePluginOptionsEditorProps<DataSourceOptions> {}
+
+interface State {}
+
+export class ConfigEditor extends PureComponent<Props, State> {
+  onClientIdChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log('client ID change', event.target.value);
+    const { onOptionsChange, options } = this.props;
+    const jsonData = {
+      ...options.jsonData,
+      clientId: event.target.value,
+    };
+    console.log('props', jsonData);
+    onOptionsChange({ ...options, jsonData });
+  };
+
+  // Secure field (only sent to the backend)
+  onClientSecretChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      secureJsonData: {
+        clientSecret: event.target.value,
+      },
+    });
+  };
+
+  onResetClientSecret = () => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      secureJsonFields: {
+        ...options.secureJsonFields,
+        apiKey: false,
+      },
+      secureJsonData: {
+        ...options.secureJsonData,
+        apiKey: '',
+      },
+    });
+  };
+
+  getGoogleAuthLink = () => {
+    const authUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+    const redirectUrl = window.location.origin + window.location.pathname;
+    const scopes = [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/fitness.activity.read',
+      'https://www.googleapis.com/auth/fitness.blood_glucose.read',
+      'https://www.googleapis.com/auth/fitness.blood_pressure.read',
+      'https://www.googleapis.com/auth/fitness.body.read',
+      'https://www.googleapis.com/auth/fitness.heart_rate.read',
+      'https://www.googleapis.com/auth/fitness.body_temperature.read',
+      'https://www.googleapis.com/auth/fitness.location.read',
+      'https://www.googleapis.com/auth/fitness.nutrition.read',
+      'https://www.googleapis.com/auth/fitness.oxygen_saturation.read',
+      'https://www.googleapis.com/auth/fitness.reproductive_health.read',
+      'https://www.googleapis.com/auth/fitness.sleep.read',
+    ];
+    const clientId = this.props.options.jsonData.clientId;
+
+    return `${authUrl}?scope=${scopes.join(
+      ' '
+    )}&include_granted_scopes=true&access_type=offline&response_type=code&redirect_uri=${redirectUrl}&client_id=${clientId}`;
+  };
+
+  render() {
+    const { options } = this.props;
+    const { jsonData, secureJsonFields } = options;
+    const secureJsonData = (options.secureJsonData || {}) as SecureJsonData;
+
+    const googleAuthLink = this.getGoogleAuthLink();
+
+    return (
+      <div className="gf-form-group">
+        <div className="gf-form">
+          <FormField
+            label="Client ID"
+            labelWidth={6}
+            inputWidth={20}
+            onChange={this.onClientIdChange}
+            value={jsonData.clientId || ''}
+          />
+        </div>
+
+        <div className="gf-form-inline">
+          <div className="gf-form">
+            <SecretFormField
+              isConfigured={(secureJsonFields && secureJsonFields.apiKey) as boolean}
+              value={secureJsonData.clientSecret || ''}
+              label="Client secret"
+              labelWidth={6}
+              inputWidth={20}
+              onReset={this.onResetClientSecret}
+              onChange={this.onClientSecretChange}
+            />
+          </div>
+        </div>
+
+        <div className="gf-form-group">
+          <a type="button" href={googleAuthLink}>
+            <img src="public/plugins/serge1peshcoff-google-fit/img/logo.png" />
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
